4bf34ab0e2e496e78d664e9c20a1516cc1dc9b63
parthwebdev <parth.webdev@gmail.com>
2023-04-14 14:50:17 +0530

    added all Files

---
+++ script.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("form");
+const input = document.querySelector("form input");
+const tasksContainer = document.querySelector(".tasks");
+
+let todos = [];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const todo = {
+    text: input.value,
+    id: new Date().getTime(),
+  };
+
+  todos.push(todo);
+  // Reset the form
+  e.target.reset();
+
+  displayTodos();
+});
+
+const displayTodos = () => {
+  tasksContainer.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const taskEl = document.createElement("div");
+    const inputEl = document.createElement("input");
+    const textEl = document.createElement("p");
+    const delButton = document.createElement("button");
+
+    taskEl.classList.add("task");
+    inputEl.classList.add("checkbox");
+    delButton.classList.add("delete");
+    textEl.classList.add("text");
+
+    inputEl.type = "checkbox";
+    textEl.innerHTML = todo.text;
+    delButton.innerHTML = "Delete";
+
+    delButton.addEventListener("click", () => {
+      todos = todos.filter((t) => t.id !== todo.id);
+      displayTodos();
+    });
+
+    taskEl.appendChild(inputEl);
+    taskEl.appendChild(textEl);
+    taskEl.appendChild(delButton);
+    tasksContainer.appendChild(taskEl);
+  });
+};
